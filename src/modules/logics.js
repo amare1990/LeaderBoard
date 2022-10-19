47e960929894f7943fce2f7358764c96ba835c55
amare1990 <amaremek@gmail.com>
2022-10-19 21:08:13 +0300

    Modify src/modules/logics.js

--- src/modules/logics.js
+++ src/modules/logics.js
@@ -1,32 +1,33 @@
-import AddScores from './Constructor.js';
+import Score from './score.js';
+import { wrapper, container, scores, recentScores, refreshBtn, showScoresTitle, scorecontainer, 
+  addScoreDiv, inputFieldsWrapper, name, score, errMessage, submitBtn } from './variables.js';
 
 const scoreBoard = document.querySelector('.score-board-ul');
 const nameInput = document.querySelector('#name');
 const scoreInput = document.querySelector('#score');
 
 const createScore = () => {
-  const name = nameInput.value;
-  const score = scoreInput.value;
-  const newScoresInput = new AddScores(name, score);
-  newScoresInput.addScore();
+  const nameValue = name.value;
+  const scoreValue = score.value;
+  const scoreObj = new Score(nameValue, scoreValue);
+  scoreObj.addScore();
   const newScoreLi = document.createElement('li');
   newScoreLi.className = 'score-board-li';
-  scoreBoard.appendChild(newScoreLi);
+  scorecontainer.appendChild(newScoreLi);
   newScoreLi.innerHTML = `
-    <p class="score-board-name">${newScoresInput.name}:</p>
-    <p class="score-board-score">${newScoresInput.score}</p>
+    <p class="score-board-name">${scoreObj.name}:</p>
+    <p class="score-board-score">${scoreObj.score}</p>
     `;
-  nameInput.value = '';
-  scoreInput.value = '';
-  localStorage.getItem('scores');
+  name.value = '';
+  score.value = '';
 };
 
 const displayScores = () => {
-  const scores = new AddScores().getScores();
+  const scores = new Score().getScores();
   scores.forEach((score) => {
     const newScoreLi = document.createElement('li');
     newScoreLi.className = 'score-board-li';
-    scoreBoard.appendChild(newScoreLi);
+    scorecontainer.appendChild(newScoreLi);
     newScoreLi.innerHTML = `
             <p class="score-board-name">${score.name}:</p>
             <p class="score-board-score">${score.score}</p>
