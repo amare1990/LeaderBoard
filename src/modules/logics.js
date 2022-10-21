4828c864f39f288eeb3209da6572d5a0d67ccb9b
amare1990 <amaremek@gmail.com>
2022-10-21 10:20:01 +0300

    Remove unnnecessary codes from this file

--- src/modules/logics.js
+++ src/modules/logics.js
@@ -1,47 +1,22 @@
-import Score from './score.js';
-import { scorecontainer, name, score } from './variables.js';
-/* import { receiveScores } from './game.js'; */
-/* import receiveScores from './game.js'; */
-//import addScore from './game.js';
-//const scoresArray = [];
-
-/* const addScore = (scoresArray) => {
-  const nameValue = name.value;
-  const scoreValue = score.value;
-  //let t = createScore(nameValue, scoreValue);
-  console.log('name value  '+ nameValue + ' '+ scoreValue);
-  //scoresArray.push(scores);
-  const scoreObj = new Score(nameValue, scoreValue);
-  sendScore(nameValue, scoreValue);
-  scoresArray.result.push(scoreObj);
-  console.log("Array "+ scoresArray);
-  displayScores(scoresArray);
- 
-  name.value = '';
-  score.value = '';
-}; */
+const scorecontainer = document.querySelector('.score-container');
 
 const displayScores = (scoresArray) => {
 
   scorecontainer.innerHTML = '';
-  for(let i = 0; i < scoresArray.length; i += 1) {
+  console.log ('Length of scoresArray= '+ scoresArray.result.length);
+ 
+  for(let i = 0; i < scoresArray.result.length; i += 1) {
     const scoreListItem = document.createElement('li');
     scoreListItem.className = 'score-item';
     const nameTag = document.createElement('p');
-    nameTag.innerHTML = scoresArray.result[i].name;
+    nameTag.innerHTML = scoresArray.result[i].user;
     const scoreTag = document.createElement('p');
     scoreTag.innerHTML = scoresArray.result[i].score;
-    console.log(scoresArray.result[i].score+ '  inside displayScore()')
     scoreListItem.append(nameTag);
     scoreListItem.append(scoreTag);
-    scorecontainer.appendChild(scoreListItem);
+    scorecontainer.append(scoreListItem);
   }
-  // score.append(scorecontainer);
   
 };
-
-/* const refreshPage = () => {
-  window.location.reload();
-}; */
 
 export { displayScores };
